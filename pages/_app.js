c8e9fc639eebd847c006cf50d5ee0ce39c7c99fb
Mani Kumar Reddy Kancharla <mani.kancharla@kmanikumarreddy@live.com>
2022-05-19 17:15:14 +0530

    added apple images

--- pages/_app.js
+++ pages/_app.js
@@ -20,15 +20,9 @@ export default function MyApp({ Component, pageProps }) {
             <meta name='msapplication-tap-highlight' content='no' />
             <meta name='theme-color' content='#000000' />
 
-            <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-            <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-            <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-            <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
+            <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
 
-            <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-            <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
             <link rel='manifest' href='/manifest.json' />
-            <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
             <link rel='shortcut icon' href='/favicon.ico' />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
 
@@ -46,14 +40,11 @@ export default function MyApp({ Component, pageProps }) {
             <meta property='og:image' content='https://google-pay-gully-cricket-cards-sharing-app.kmanikumarreddy.com/icons/apple-touch-icon.png' />
 
             <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
+            <link rel='apple-touch-startup-image' href='/images/apple_splash_1620.png' sizes='1620x2160' />
             <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
             <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
             <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
             <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
-
-            <meta name='og:image' content="https://www.coolztricks.com/wp-content/uploads/2021/09/photo_2022-05-13-09.59.14.jpeg" />
 
         </Head>
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TK2PY77NH7" strategy="afterInteractive"></Script>
