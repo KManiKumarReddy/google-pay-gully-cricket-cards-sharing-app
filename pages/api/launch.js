19e19774cff5c557cf1cce59f2370d65106476b2
Mani Kumar Reddy Kancharla <mani.kancharla@kmanikumarreddy@live.com>
2022-05-22 09:05:46 +0530

    Revert "added launch api"
    This reverts commit 5729f2af2a8dd6a7f39f72de8aa02b5704e8ffed.

--- pages/api/launch.js
+++
@@ -1,9 +0,0 @@
-import { deleteFirstColumnInEachSheet } from "../../services/sheets"
-
-
-const handler = async (req, res) => {
-    await deleteFirstColumnInEachSheet()
-    res.status(200).json({ success: true })
-}
-
-export default handler
